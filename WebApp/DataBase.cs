67f34e29f5f987cf6fc9fe84b37679ab3ccc95b1
Ilya <ilya.voloshin.16@mail.ru>
2024-06-07 01:58:34 +0500

    Database connection added

---
+++ WebApp/DataBase.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace WebApp
+{
+    public class DataBase
+    {
+        MySqlConnection _connection = new MySqlConnection("server=localhost;port=3306;username=root;password=Bk.[fDjkj[f03;database=institutes");
+
+        public void openConnection()
+        {
+            if (_connection.State == System.Data.ConnectionState.Closed) { _connection.Open(); }
+        }
+
+        public void closeConnection()
+        {
+            if (_connection.State == System.Data.ConnectionState.Open) { _connection.Close(); }
+        }
+
+        public MySqlConnection getConnection()
+        {
+            return _connection;
+        }
+    }
+}
--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63c277e0a0ebb6f12bea49bfbfb970c2bf36504a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+986901ba280a78ae364b649d624d3c2b063f2b4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
