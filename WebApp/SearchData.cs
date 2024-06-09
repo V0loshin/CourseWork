8c9f8c33146194afeb17ea32b1da9823a3806b55
Ilya <ilya.voloshin.16@mail.ru>
2024-06-09 20:55:48 +0500

    Added functions and frontend

---
+++ WebApp/SearchData.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApp
+{
+	public class SearchData
+	{
+		public string Name { get; set; } = "";
+		public Dictionary<string, bool> EducFormVals { get; set; }
+		public int MinScore { get; set; } = 0;
+		public int MaxScore { get; set; } = 400;
+		public int BudgetPlaces { get; set; } = 0;
+		public int ContractPlaces { get; set; } = 0;
+		public int MinCost { get; set; } = 0;
+		public int MaxCost { get; set; } = 300000;
+		public Dictionary<int, int> SortTypesSet { get; set; }
+	}
+}
--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+986901ba280a78ae364b649d624d3c2b063f2b4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67f34e29f5f987cf6fc9fe84b37679ab3ccc95b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
